6c576e7cb955d49ee308dd2120c80400c2cab224
Realgotqkura <vladianastasov06@gmail.com>
2022-08-09 17:50:16 +0300

    Initial commit

---
+++ src/com/realgotqkura/main/Main.java
@@ -0,0 +1,19 @@
+package com.realgotqkura.main;
+
+public class Main {
+
+
+    public Main(){
+        run();
+    }
+
+
+    public void run(){
+
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
